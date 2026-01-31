bf1e52fe9f95945e8ee710ea731b56fce960f4e5
James Acres <jamesacres@gmail.com>
2026-01-31 19:33:58 +0000

    fix: mascot

--- apps/jamesacres/src/app/page.tsx
+++ apps/jamesacres/src/app/page.tsx
@@ -18,12 +18,11 @@ export default async function Page() {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-8 pb-8 pt-6 md:space-y-10">
-          <div className="flex flex-col gap-6 md:flex-row md:items-center md:gap-8">
-            <div className="relative flex-1">
-              <div className="animate-fade-in relative rounded-lg bg-pink-100 p-6 md:p-8 dark:bg-pink-900/30">
-                <div className="absolute -right-2 top-1/2 h-0 w-0 border-b-[12px] border-l-[16px] border-t-[12px] border-b-transparent border-l-pink-100 border-t-transparent md:hidden dark:border-l-pink-900/30"></div>
-                <div className="absolute -bottom-2 left-1/2 hidden h-0 w-0 border-l-[12px] border-r-[12px] border-t-[16px] border-l-transparent border-r-transparent border-t-pink-100 md:block dark:border-t-pink-900/30"></div>
-                <p className="text-xl font-semibold leading-8 text-gray-900 dark:text-gray-100">
+          <div className="flex flex-row items-start justify-end gap-4">
+            <div className="relative mt-20 max-w-lg">
+              <div className="animate-fade-in relative rounded-lg bg-pink-400 p-4 dark:bg-pink-500">
+                <div className="absolute -right-2 top-1/2 h-0 w-0 border-b-[12px] border-l-[16px] border-t-[12px] border-b-transparent border-l-pink-400 border-t-transparent dark:border-l-pink-500"></div>
+                <p className="whitespace-nowrap text-lg font-semibold leading-7 text-white">
                   I'm creating awesome, here are some of my thoughts.
                 </p>
               </div>
@@ -32,9 +31,9 @@ export default async function Page() {
               <Image
                 src="/static/images/mascot.png"
                 alt="avatar"
-                width={192}
-                height={192}
-                className="h-48 w-48"
+                width={150}
+                height={300}
+                className="h-auto w-36"
               />
             </div>
           </div>
