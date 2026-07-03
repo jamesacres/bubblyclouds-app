192e23e6748a83ff515cf90098c8664df4fc03ae
James Acres <jamesacres@gmail.com>
2026-07-03 20:02:38 +0100

    fix: add new types

---
+++ packages/types/src/loginContext.ts
@@ -0,0 +1,11 @@
+/**
+ * Enum for login modal contexts
+ * Used to provide specific messaging based on which action triggered sign-in
+ */
+export enum LoginContext {
+  DAILY_PUZZLE = 'dailyPuzzle',
+  PUZZLE_BOOK = 'puzzleBook',
+  JOIN_TEAM = 'joinTeam',
+  SUBSCRIBE = 'subscribe',
+  RACE_LOBBY = 'raceLobby',
+}
